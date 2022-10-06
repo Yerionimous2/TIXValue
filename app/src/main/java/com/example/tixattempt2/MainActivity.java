d3f88807d4a36dd59d94fdae6094ba5b8cb8ab02
Yerionimous2 <pminor@uni-muenster.de>
2022-10-06 21:45:22 +0200

    Implemented funktions to get Values of the important etfs. The Modifiers in GetY are still off.

--- app/src/main/java/com/example/tixattempt2/MainActivity.java
+++ app/src/main/java/com/example/tixattempt2/MainActivity.java
@@ -46,8 +46,6 @@ public class MainActivity extends AppCompatActivity {
         grStonks = findViewById(R.id.graph1);
         lbValue = findViewById(R.id.lbValue);
         x = 0;
-        performGetCall("https://www.google.com/search?q=s%26p+500+wert&oq=s%26p+500+wert&aqs=chrome..69i57.10719j0j7&sourceid=chrome&ie=UTF-8");
-        getSP500Value();
         tmTk1 = new TimerTask() {
 
 
@@ -55,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 x += 2;
                 y = getY();
-                y += (Math.random()*2-1)/200;
                 yString = y + " Euro";
                 runOnUiThread(() -> lbValue.setText(yString));
                 editor.putFloat("Value", y);
@@ -101,8 +98,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public float getSP500Value() {
-        String htmlTemp2 = performGetCall("https://www.google.com/search?q=s%26p+500+wert&oq=s%26p+500+wert&aqs=chrome..69i57.10719j0j7&sourceid=chrome&ie=UTF-8");
+    public float getXiaomiValue() {
+        String htmlTemp2 = performGetCall("https://www.google.com/search?q=xiaomi+wert&oq=xiaomi+wert&aqs=chrome..69i57.2502j0j7&sourceid=chrome&ie=UTF-8");
         int index = 0;
         for(int i = 14; i < htmlTemp2.length(); i++) {
             if(htmlTemp2.startsWith("jsname=\"vWLAgc", i - 14)) {
@@ -111,31 +108,79 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        String valueString = htmlTemp2.substring(index + 38, index + 46);
+        String valueString = getNumberFrom(htmlTemp2.substring(index + 36, index + 48));
 
         return getValueFromString(valueString);
     }
 
-    public float getValueFromString(String valueString) {
-        char[] valueArray = new char[valueString.length() - 1];
-
-        for(int i = 0; i < valueString.length() - 1; i++) {
-            if(i >= 1) {
-                if(valueString.toCharArray()[i+1] != ',') {
-                    valueArray[i] = valueString.toCharArray()[i+1];
-                } else {
-                    valueArray[i] = '.';
-                }
-            } else {
-                valueArray[i] = valueString.toCharArray()[i];
+    public float getGlobalCleanEnergyValue() {
+        String htmlTemp2 = performGetCall("https://www.justetf.com/de/etf-profile.html?isin=IE00B1XNHC34#overview");
+        int index = 0;
+        for(int i = 5; i < htmlTemp2.length(); i++) {
+            if(htmlTemp2.startsWith("<span>EUR</span>", i - 5)) {
+                index = i;
+                break;
             }
         }
-        valueString = new String(valueArray);
 
+        String valueString = getNumberFrom(htmlTemp2.substring(index + 47, index + 57));
+
+        return getValueFromString(valueString);
+    }
+
+    public float getSP500Value() {
+        String htmlTemp2 = performGetCall("https://www.finanzen.net/etf/ishares-global-clean-energy-etf-ie00b1xnhc34");
+        int index = 0;
+        for(int i = 14; i < htmlTemp2.length(); i++) {
+            if(htmlTemp2.startsWith("<span>EUR</span>", i - 14)) {
+                index = i;
+                break;
+            }
+        }
+
+        String valueString = getNumberFrom(htmlTemp2.substring(index - 20, index - 10));
+
+        return getValueFromString(valueString);
+    }
+
+    public String getNumberFrom(String input) {
+        char[] inputArray = input.toCharArray();
+        char[] outputArray = new char[input.length()];
+        int j = 0;
+        for(int i = 0; i < input.length(); i++) {
+            switch(inputArray[i]){
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    outputArray[j] = inputArray[i];
+                    j++;
+                    break;
+                case ',':
+                    outputArray[j] = '.';
+                    j++;
+                    break;
+            }
+        }
+        return new String(outputArray);
+    }
+
+    public float getValueFromString(String valueString) {
         return Float.parseFloat(valueString);
     }
 
     public float getY() {
-        return getSP500Value();
+        float result = 0;
+        result += 1000 * getXiaomiValue();
+        result += 75   * getGlobalCleanEnergyValue();
+        result += 100  * getSP500Value();
+        result /= 274;
+        return result;
     }
 }
