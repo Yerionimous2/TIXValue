7ef00639d66374bb6e4b6a52b5994dfb28fbb3b2
Yerionimous2 <pminor@uni-muenster.de>
2022-10-06 19:05:38 +0200

    The Graph and the Values are now tied to the S&P 500 etf

--- app/src/main/java/com/example/tixattempt2/MainActivity.java
+++ app/src/main/java/com/example/tixattempt2/MainActivity.java
@@ -9,6 +9,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     public TimerTask tmTk1;
     public TextView lbValue;
     private String yString;
+    private static String htmlTemp = "";
     private float x;
     private float y;
 
@@ -40,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         grStonks = findViewById(R.id.graph1);
         lbValue = findViewById(R.id.lbValue);
         x = 0;
+        performGetCall("https://www.google.com/search?q=s%26p+500+wert&oq=s%26p+500+wert&aqs=chrome..69i57.10719j0j7&sourceid=chrome&ie=UTF-8");
+        getSP500Value();
         tmTk1 = new TimerTask() {
 
 
@@ -60,7 +67,70 @@ public class MainActivity extends AppCompatActivity {
         tm1.schedule(tmTk1, 0, 2000);
     }
 
+    public static String performGetCall(String sUrl) {
+        Thread t = new Thread(() -> {
+            try {
+                URL url = new URL(sUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(15000);
+                connection.setReadTimeout(15000);
+                connection.connect();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String content = "", line;
+                while ((line = rd.readLine()) != null) {
+                    content += line + "\n";
+                }
+                htmlTemp = content;
+            } catch(Exception ex) {
+                htmlTemp = "";
+            }
+        });
+        t.start();
+        while(t.isAlive());
+        //System.out.println(htmlTemp);
+
+        return htmlTemp;
+    }
+
+    public float getSP500Value() {
+        String htmlTemp2 = performGetCall("https://www.google.com/search?q=s%26p+500+wert&oq=s%26p+500+wert&aqs=chrome..69i57.10719j0j7&sourceid=chrome&ie=UTF-8");
+        int index = 0;
+        for(int i = 14; i < htmlTemp2.length(); i++) {
+            if(htmlTemp2.substring(i - 14,i).equals("jsname=\"vWLAgc")) {
+                index = i;
+                break;
+            }
+        }
+
+        String valueString = htmlTemp2.substring(index + 38, index + 46);
+        float value = getValueFromString(valueString);
+
+        return value;
+    }
+
+    public float getValueFromString(String valueString) {
+        char[] valueArray = new char[valueString.length() - 1];
+
+        for(int i = 0; i < valueString.length() - 1; i++) {
+            if(i >= 1) {
+                if(valueString.toCharArray()[i+1] != ',') {
+                    valueArray[i] = valueString.toCharArray()[i+1];
+                } else {
+                    valueArray[i] = '.';
+                }
+            } else {
+                valueArray[i] = valueString.toCharArray()[i];
+            }
+        }
+        valueString = new String(valueArray);
+
+        float value = Float.parseFloat(valueString);
+
+        return value;
+    }
+
     public float getY(float y) {
-        return (float) (y + (Math.random()*2-1)/200);
+        return getSP500Value();
     }
 }
